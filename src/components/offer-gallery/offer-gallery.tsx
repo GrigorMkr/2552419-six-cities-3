51f500905723e5f16f1a9ea6d59aa8f68a06faef
GrigorMkr <grom666@gmail.com>
2025-12-22 22:15:08 +0500

    7.14. Истина где-то на сервере

--- src/components/offer-gallery/offer-gallery.tsx
+++ src/components/offer-gallery/offer-gallery.tsx
@@ -1,19 +1,18 @@
 import { FC } from 'react';
+import { getImageUrl } from '../../utils/image-url';
 
 type OfferGalleryProps = {
   images: string[];
 }
 
 const OfferGallery: FC<OfferGalleryProps> = ({images}) => {
-  const baseUrl = import.meta.env.BASE_URL || '';
-
   return (
     <div className="offer__gallery-container container">
       <div className="offer__gallery">
         {images.map((image, index) => (
           // eslint-disable-next-line react/no-array-index-key
           <div key={index} className="offer__image-wrapper">
-            <img className="offer__image" src={`${baseUrl}${image}`} alt="Photo studio" />
+            <img className="offer__image" src={getImageUrl(image)} alt="Photo studio" />
           </div>
         ))}
       </div>
