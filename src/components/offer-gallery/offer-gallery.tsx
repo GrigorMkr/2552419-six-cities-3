973739e1dd7d19b8f41ca13dc1308f0ae9a4ea5a
GrigorMkr <grom666@gmail.com>
2025-12-22 22:18:10 +0500

    ошибку  линтера правит

--- src/components/offer-gallery/offer-gallery.tsx
+++ src/components/offer-gallery/offer-gallery.tsx
@@ -5,20 +5,18 @@ type OfferGalleryProps = {
   images: string[];
 }
 
-const OfferGallery: FC<OfferGalleryProps> = ({images}) => {
-  return (
-    <div className="offer__gallery-container container">
-      <div className="offer__gallery">
-        {images.map((image, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={index} className="offer__image-wrapper">
-            <img className="offer__image" src={getImageUrl(image)} alt="Photo studio" />
-          </div>
-        ))}
-      </div>
+const OfferGallery: FC<OfferGalleryProps> = ({images}) => (
+  <div className="offer__gallery-container container">
+    <div className="offer__gallery">
+      {images.map((image, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={index} className="offer__image-wrapper">
+          <img className="offer__image" src={getImageUrl(image)} alt="Photo studio" />
+        </div>
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default OfferGallery;
 
